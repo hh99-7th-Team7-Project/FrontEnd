c322d6f15d6d0b3b2e40fff3e99c930664f931e0
Seohyun <rmsid1000@gmail.com>
2022-06-30 15:39:19 +0900

    [최서현] 전체 프레임 양 옆마진

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.css';
 import Router from './shared/Router';
+import styled from 'styled-components'
 
 
 function App() {
   return (
-    <> 
+    <SCwrap> 
         <Router/>
-    </>
+    </SCwrap>
   );
 }
 
 
 
 export default App;
+
+const SCwrap = styled.div`
+margin: 0 15%;
+`
