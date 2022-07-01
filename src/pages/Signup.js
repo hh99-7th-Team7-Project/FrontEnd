41f4637e70e9a7aa3f95b83e716cb5b8fb542958
Seohyun <rmsid1000@gmail.com>
2022-07-01 19:56:48 +0900

    [최서현] 회원가입 로직 변경중

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import apis from "../shared/api/main";
 import styled from "styled-components";
+import axios from 'axios';
 
 
 const SignUp = () => {
@@ -11,7 +12,7 @@ const SignUp = () => {
   const [Password2, setPassword2] = useState("");
   const [Nickname, setNickname] = useState("");
   const [fileImage, setFileImage] = React.useState("");
-  const [ProfileImage,setProfileImage] = React.useState([])
+  const [ProfileImage, setProfileImage] = React.useState([])
 
   const fileInputRef = React.useRef();
   const passwordRef = React.useRef();
@@ -19,44 +20,60 @@ const SignUp = () => {
   const emailRef = React.useRef();
   const nicknameRef = React.useRef();
   const check = React.useRef();
-  const [okid, setOkid] = React.useState('false');
-  const [oknickname, setokNickname] = React.useState('false');
+  const [okid, setOkid] = React.useState(false);
+  const [oknickname, setokNickname] = React.useState(false);
 
+
+  console.log(fileImage)
   // 이메일 중복 체크
   const dupEmail = async () => {
-    await apis.checkEmail({ username: Email })
-      .then((res) => {
-        window.alert("사용가능한 아이디 입니다.");
-        setOkid(true);
-      })
-      .catch((error) => {
-        window.alert("이미 사용중인 아이디입니다.");
-        // console.log("Login Error", error);
-      });
+    if (!idCheck(Email)) {
+      window.alert("이메일 형식이 아닙니다")
+    } else {
+      await apis.checkEmail({ username: Email })
+        .then((res) => {
+          console.log(res)
+          if (res.data) {
+            window.alert("사용가능한 메일 입니다.");
+            setOkid(true);
+          } else {
+            window.alert("이미 사용중인 메일입니다.");
+          }
+        })
+        .catch((error) => {
+
+          // console.log("Login Error", error);
+        });
+    }
+
   };
 
   // 닉네임 중복 체크
   const dupNick = async () => {
-    await apis.checkNickName({ nickname: Nickname })
-      .then((res) => {
-        window.alert("사용가능한 닉네임 입니다.");
-        setokNickname(true);
-      })
-      .catch((error) => {
-        window.alert("이미 사용중인 닉네임입니다.");
-        // console.log("Login Error", error);
-      });
+    if (!nickCheck(Nickname)) {
+      window.alert("올바른 닉네임 형식을 작성해주세요")
+    } else {
+      await apis.checkNickName({ nickname: Nickname })
+        .then((res) => {
+          if (res.data) {
+            window.alert("사용가능한 닉네임 입니다.");
+            setokNickname(true);
+          } else {
+            window.alert("이미 사용중인 닉네임입니다.");
+          }
+        })
+    }
   };
 
   if (Password && Password2 && Password === Password2) {
     check.current.innerText = "✔";
   } else if (Password !== Password2) {
-    check.current.innerText = "✖";
+    check.current.innerText = "✖ 비밀번호가 일치하지 않습니다";
   }
 
   //아이디,비번,닉네임 정규식
   const idCheck = (email) => {
-    let regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    let regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/;
     // 대문자 포함
     return regExp.test(email);
   };
@@ -83,10 +100,6 @@ const SignUp = () => {
       window.alert("아이디,비밀번호,닉네임을 모두 입력해주세요!");
       return;
     }
-    if (!idCheck(Email)) {
-      window.alert("형식을 지켜주세요.");
-      return;
-    }
     if (!pwCheck(Password, Password2)) {
       window.alert("숫자 및 영어만 입력가능합니다.");
       return;
@@ -95,44 +108,44 @@ const SignUp = () => {
       window.alert("비밀번호 불일치 : 고새 까먹었어?");
       return;
     }
-    if (!nickCheck(Nickname)) {
-      window.alert("올바른 닉네임 형식을 작성해주세요");
-      return;
-    }
     if (!(okid && oknickname)) {
       window.alert("중복체크를 모두 해주세요!")
+    } else {
+      const form = new FormData();
+      const datas = {
+        username: emailRef.current.value,
+        nickname: nicknameRef.current.value,
+        password: passwordRef.current.value,
+      }
+      form.append("signup", new Blob([JSON.stringify(datas)], {
+        type: "application/json"
+      }))
+      form.append('profileImage', fileInputRef.current.files[0])
+      console.log(form)
+
+      const res = await apis.addUser(form);
+      // console.log(res);
+      // alert(res.data.body.message);
+      navigate("/login")
     }
-
-
-    const form = new FormData();
-    form.append('userProfileImage', ProfileImage)
-    form.append('username', emailRef.current.value)
-    form.append('nickname', nicknameRef.current.value)
-    form.append('password', passwordRef.current.value)
-    form.append('passwordck', password2Ref.current.value)
-    const res = await apis.addUser({
-      form
-    });
-    // console.log(res);
-    // alert(res.data.body.message);
-    navigate("/login")
+    // console.log(ProfileImage[0])
   };
   //프로필 사진 업로드
   const saveFileImage = async (e) => {
     setFileImage(URL.createObjectURL(e.target.files[0]));
-    setProfileImage(e.target.files)
+    // setProfileImage(e.target.files)
   };
 
-console.log(ProfileImage)
+  // console.log(ProfileImage)
 
   return (
     <div>
       <section>
         <main>
-          <form onSubmit={onSubmitUserHandler} style={{ marginTop: "30px" }}>
-            <Wrap>
-              <Inputbox
-                type="text"
+          <div style={{ marginTop: "30px" }}>
+            <ScWrap>
+              <ScInputbox
+                type="email"
                 placeholder="Email"
                 ref={emailRef}
                 value={Email}
@@ -141,12 +154,12 @@ console.log(ProfileImage)
                 }}
                 style={{ marginRight: "10px" }}
               />
-              <DupliButton onClick={dupEmail}>중복확인</DupliButton>
-            </Wrap>
-            <Condition>E-mail주소를 입력해 주세요</Condition>
+              <ScDuplicateButton onClick={dupEmail}>중복확인</ScDuplicateButton>
+            </ScWrap>
+            <ScCondition>E-mail주소를 입력해 주세요</ScCondition>
             <br />
-            <Wrap>
-              <Inputbox
+            <ScWrap>
+              <ScInputbox
                 type="text"
                 placeholder="Nickname"
                 ref={nicknameRef}
@@ -156,11 +169,11 @@ console.log(ProfileImage)
                 }}
                 style={{ marginRight: "10px" }}
               />
-              <DupliButton onClick={dupNick}>중복확인</DupliButton>
-            </Wrap>
-            <Condition>당신이 불리고 싶은 이름을 입력해주세요</Condition>
+              <ScDuplicateButton onClick={dupNick}>중복확인</ScDuplicateButton>
+            </ScWrap>
+            <ScCondition>당신이 불리고 싶은 이름을 입력해주세요</ScCondition>
             <br />
-            <Inputbox
+            <ScInputbox
               type="password"
               placeholder="Password"
               value={Password}
@@ -170,10 +183,10 @@ console.log(ProfileImage)
               ref={passwordRef}
               style={{ marginRight: "110px" }}
             />
-            <Condition>비밀번호는 8자 이상 영문과 숫자로만 만들어 주세요</Condition>
+            <ScCondition>비밀번호는 8자 이상 영문과 숫자로만 만들어 주세요</ScCondition>
             <br />
 
-            <Inputbox
+            <ScInputbox
               type="password"
               placeholder="Password check"
               value={Password2}
@@ -183,8 +196,8 @@ console.log(ProfileImage)
               ref={password2Ref}
               style={{ marginRight: "110px" }}
             />
-           <p ref={check} />
-            <Condition>비밀번호를 다시 입력해주세요</Condition>
+            <div ref={check} />
+            <ScCondition>비밀번호를 다시 입력해주세요</ScCondition>
             <br />
             <h5 style={{ marginLeft: "70px" }}>프로필 사진</h5>
             {fileImage && (
@@ -192,17 +205,17 @@ console.log(ProfileImage)
                 alt="sample"
                 src={fileImage}
                 style={{ margin: "10px auto 7px 80px", maxWidth: "300px", maxHeight: "250px" }}
+                multiple="multiple"
               />)}
-            {fileImage ? (null) : (<Inputbox
+            <ScInputbox
               name="imgUpload"
               type="file"
               accept="image/*"
               ref={fileInputRef}
               onChange={saveFileImage}
-            />)}
-            <LoginButton onClick={onSubmitUserHandler}>가입하기</LoginButton>
-            {/* {okid === true && oknickname === true ? (<LoginButton onClick={onSubmitUserHandler}>가입하기</LoginButton>) : null} */}
-          </form>
+            />
+            <ScLoginButton onClick={onSubmitUserHandler}>가입하기</ScLoginButton>
+          </div>
         </main>
       </section>
     </div>
@@ -210,7 +223,7 @@ console.log(ProfileImage)
   );
 };
 
-const Inputbox = styled.input`
+const ScInputbox = styled.input`
   display: flex;
   align-items: center;
   flex-direction: column;
@@ -223,19 +236,20 @@ const Inputbox = styled.input`
   color: rgb(74,21,75);
 `
 
-const DupliButton = styled.button`
+const ScDuplicateButton = styled.div`
   background-color: rgb(74,21,75);
   color: white;
   padding: 6px 10px;
   border-radius: 4px;
   font-size: 12px;
   margin: 0 30px 0 0;
+  cursor: pointer;
 `
 
-const Wrap = styled.div`
+const ScWrap = styled.div`
   display: flex;
 `
-const LoginButton = styled.button`
+const ScLoginButton = styled.button`
 padding: 7px 10px;
 display: flex;
 margin: 20px  auto 8px auto;
@@ -248,7 +262,7 @@ border-radius: 5px;
 
 `
 
-const Condition = styled.h6`
+const ScCondition = styled.h6`
   margin: 6px 0 10px 78px;
   text-align: left;
 
