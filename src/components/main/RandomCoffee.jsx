5bb0d3f8928752b1304a38264d7ac21dc06c83ca
Seohyun <rmsid1000@gmail.com>
2022-07-02 15:35:44 +0900

    [최서현] 메인 커피 땡겨오기

--- src/components/main/RandomCoffee.jsx
+++ src/components/main/RandomCoffee.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Styled from 'styled-components'
+import apis from '../../shared/api/main'
 
 const RandomCoffee = () => {
 
+  // useEffect(()=>{
+  //   apis.getRandomCoffee()
+  // },[])
 
 
 
--- src/shared/api/main.js
+++ src/shared/api/main.js
@@ -1,4 +1,4 @@
-import {api,instances} from "./core/api";
+import {api,instance,instances} from "./core/api";
 
 const apis = {
 
@@ -7,7 +7,10 @@ const apis = {
     addUser: (newUser) => instances.post("/api/signup", newUser),
     postLogin : (userdata) => api.post("/api/login", userdata),
 
-    getCoffee: () => api.get("/coffee")
+    getCoffee: () => api.get("/coffee"),
+    getCoffees: ()=> api.get("/coffee"),
+    addCoffee: (brand, coffee) => instance.post(`/coffee/${brand}`,coffee),
+    getRandomCoffee: ()=> api.get("/coffee/random?brand=스타벅스&category=coffee")
 }
 
 export default apis;
