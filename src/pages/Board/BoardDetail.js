2e45195990e917491b92277f1652decb44dcb166
Seohyun <rmsid1000@gmail.com>
2022-07-05 20:22:40 +0900

    [최서현] 페이지 추가

---
+++ src/pages/Board/BoardDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BoardDetail = () => {
+  return (
+    <div>BoardDetail</div>
+  )
+}
+
+export default BoardDetail
---
+++ src/pages/Search/SearchBoard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchBoard = () => {
+  return (
+    <div>SearchBoard</div>
+  )
+}
+
+export default SearchBoard
