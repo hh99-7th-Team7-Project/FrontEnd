92752fb465f6fbb3eb387e3a174d1b61adee3ebe
Seohyun <rmsid1000@gmail.com>
2022-07-09 03:37:40 +0900

    [최서현]

--- src/pages/Board/BoardDetail.js
+++ src/pages/Board/BoardDetail.js
@@ -6,14 +6,8 @@ import BoardMain from '../../components/BoardDetail/BoardMain';
 import BoardImage from '../../components/BoardDetail/BoardImage';
 import BoardLike from '../../components/BoardDetail/BoardLike';
 import BoardComment from '../../components/BoardDetail/BoardComment';
-import { useDispatch,useSelector } from 'react-redux';
-import { loadBoard } from '../../redux/modules/board'
 
 const BoardDetail = () => {
-  const dispatch = useDispatch()
-  // const boardReducer = useSelector((state)=>state.board.board)
-  // console.log(boardReducer)
-
   const [loading, setLoading] = useState(false);
   const [content, setContent] = useState();
   
@@ -22,8 +16,8 @@ const BoardDetail = () => {
         const getMark = async () => {
             await axios.get("http://localhost:4000/Review")
           .then((res)=>{
-            setContent(res?.data[2])
-            console.log(res?.data[2])
+            setContent(res?.data[3])
+            // console.log(res?.data[3])
             })
           }
         getMark()
