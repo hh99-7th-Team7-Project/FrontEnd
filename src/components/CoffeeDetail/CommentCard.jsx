b024dc4130b6fd788f517e12cc4478be11cf3f08
yunjinlee <jgsjfsyj@gmail.com>
2022-07-09 22:18:14 +0900

    [이윤진] 리뷰댓글 warning 해결(map key값 변경)

--- src/components/CoffeeDetail/CommentCard.jsx
+++ src/components/CoffeeDetail/CommentCard.jsx
@@ -15,7 +15,7 @@ const CommentCard = (props) => {
   
   const review_list = useSelector((state)=>state.comment.posts);
 
-  console.log(review_list);
+  console.log(review_list)
 
  
   useEffect(()=>{
@@ -24,41 +24,38 @@ const CommentCard = (props) => {
 
 
     return (
-      <>
-        <ScWrap>
-          {review_list && review_list.map((item) => {
-            return (              
-            <div>            
-              <ScComment key={item?.id}>
-                <div>
-                  <p>닉네임 : {item?.nickname}</p>
-                  <p>댓글 : {item?.review}</p>
-                </div> 
-                <ScHR/>
-                <ScButton onClick={()=>{
-                  setShowUpdate(true);
-                  setReviewId(item?.id);
-                  dispatch(__loadComment({brand, boardId}));                             
-                }}>수정</ScButton>
-                <ScButton onClick={()=>{
-                  dispatch(__deleteComment(brand,boardId,Number(item?.id)))                
-                  dispatch(__loadComment({brand, boardId}));
-                }}>삭제</ScButton>
-                {showUpdate === true ? (
-              <UpdateComment
-                showUpdate={showUpdate}
-                setShowUpdate={setShowUpdate}
-                commentId={Number(reviewId)}
-                boardId={boardId}
-                brand={brand}        
-              />) : null}  
-              </ScComment>
-            </div>
-          )})}
-        </ScWrap>
-              
+        <>
+          <ScWrap>
+            {review_list && review_list.map((item) => {
+              return (
+                <ScComment key={item?.id}>
+                  <div>
+                    <p>닉네임 : {item?.nickname}</p>
+                    <p>댓글 : {item?.review}</p>
+                  </div> 
+                  <ScHR/>
+                  <ScButton onClick={()=>{
+                    setShowUpdate(true);
+                    setReviewId(item?.id);
+                    dispatch(__loadComment({brand, boardId}));                             
+                  }}>수정</ScButton>
+                  <ScButton onClick={()=>{
+                    dispatch(__deleteComment(brand,boardId,Number(item?.id)))                
+                    dispatch(__loadComment({brand, boardId}));
+                  }}>삭제</ScButton>
+                  {showUpdate === true ? (
+                <UpdateComment
+                  showUpdate={showUpdate}
+                  setShowUpdate={setShowUpdate}
+                  commentId={Number(reviewId)}
+                  boardId={boardId}
+                  brand={brand}        
+                />) : null}  
+                </ScComment>
+            )})}
+          </ScWrap>
         </>
-        )
+      )
         
   }
 
@@ -70,7 +67,6 @@ width: 70vw;
 height: 500px;
 overflow-z: scroll;
 padding: 20px;
-border: 1px solid black;
 `;
 
 const ScComment = Styled.div`
