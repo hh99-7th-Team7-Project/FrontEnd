0c419aacda66d94c0794e7ed69f2a21395fa8e84
yunjinlee <jgsjfsyj@gmail.com>
2022-07-15 18:08:18 +0900

    [이윤진] 헤더페이지 import 통합

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import Styled from 'styled-components';
-import Logo from '../../components/Header/Logo';
-import Category from '../../components/Header/Category';
-import HeaderInput from '../../components/Header/HeaderInput';
-import Button from '../../components/Header/Button';
+import { Button, Category, HeaderInput, Logo } from '../../components/Header/A-HeaderIndex';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
