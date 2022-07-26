6ac13c880547ad657c28f9b6f2e18110bed548ed
yunjinlee <jgsjfsyj@gmail.com>
2022-07-27 04:07:26 +0900

    [이윤진]  반응형 적용중

--- src/components/board/BoardList.jsx
+++ src/components/board/BoardList.jsx
@@ -65,8 +65,14 @@ const BoardList = () => {
 
 const ScWrap = Styled.div`
     /* border: 1px solid black; */
-    width: 100%;
-    height: 100%;
+    max-width: 1200px;
+    width: 90%;
+    margin: auto;
+    @media screen and (max-width: 768px){
+    margin: auto;
+    width: 70%;
+    padding: 10px 30px 0px 0px;
+  }
 `;
 const ScBoard = Styled.div`    
     width: 100%;
